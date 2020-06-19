ae54f97592d2eb0170909f9d179ecdba9884354d
momocow <momocow.me@gmail.com>
2020-06-19 18:59:44 +0800

    :construction: Joi schema for headers.
    [skip ci]

--- lib/headers/schema.ts
+++ lib/headers/schema.ts
@@ -1,5 +1,42 @@
-import joi from 'joi'
+import libjoi from '@hapi/joi'
+import { semver } from 'joi-extension-semver'
 
-export const headerSchema = joi.object({
-  
+const Joi: libjoi.Root = libjoi.extend(semver)
+
+export const headerSchema = Joi.object({
+  name: Joi.string().default(Joi.ref('$pkg.name')).required(),
+  version: Joi.semver().default(Joi.ref('$pkg.version')).required(),
+  description: Joi.string().default(Joi.ref('$pkg.description')),
+  author: Joi.string().default(Joi.ref('$pkg.author')),
+  namespace: Joi.string(),
+  collaborator: Joi.array().items(Joi.string()).single()
+    .default(Joi.ref('$pkg.contributors')),
+  copyright: Joi.string(),
+  license: Joi.string(),
+  homepage: Joi.string(),
+  homepageURL: Joi.string(),
+  website: Joi.string(),
+  source: Joi.string(),
+  icon: Joi.string(),
+  iconURL: Joi.string(),
+  defaulticon: Joi.string(),
+  icon64: Joi.string(),
+  icon64URL: Joi.string(),
+  updateURL: Joi.string(),
+  downloadURL: Joi.string(),
+  installURL: Joi.string(),
+  supportURL: Joi.string(),
+
+  // include
+  // exclude
+  // match
+  // require
+  // resource
+  // 'run-at'
+  // grant
+  // noframes
+  // unwrap
+  // connect
+  // webRequest
+  // nocompat
 })
